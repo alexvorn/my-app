ff06102d560e432dd7cfd0ea1a76691c7f63a171
Alexvorn <voroncov.sasha93@gmail.com>
2022-01-17 16:52:28 +0200

    registration  page change to login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {login as loginAction } from '../../actions/login';
+import { login as loginAction } from '../../actions/login';
 
 class Login extends Component {
 
     loginUser = () => {
         const user = {
-            email: 'andrejstarusev@gmail.com',
-            id: '1',
+            email: '',
+            password: '',
         }
-        console.log('email', this.emailInput.value, 'password', this.createPassword.value)
+        user.email = this.emailInput.value;
+        user.password = this.passwordInput.value;
+        console.log(user)
 
         this.props.loginUser(user);
     }
 
     render() {
+        console.log(this.props)
         return (
             <div className='login-page container'>
                 <div>
@@ -22,18 +25,18 @@ class Login extends Component {
                 </div>
                 <div>
                     <img src={require('../../images/logo.png')}/>
-                    <h1>Sign Up</h1>
+                    <h1>Login</h1>
                     <div className='registration-form'>
                         <div>
                             <form action="">
                                 <span>Your email</span>
                                 <input type="text" placeholder="Email" ref={(input) => { this.emailInput = input}}/>
-                                <input type="password" placeholder="Create your password" ref={(input) => { this.createPassword = input}}/>
-                                <input type="password" placeholder="Confirm your password" ref={(input) => { this.confirmPassword = input}}/>
-                                <input  className="btn-sing-up" type="submit" value="Sing up" onClick={this.loginUser}/>
+                                <input type="password" placeholder="Your password" ref={(input) => { this.passwordInput = input}}/>
+                                <a href="">Forgot password?</a>
+                                <input  className="btn-login-up" type="submit" value="Login" onClick={this.loginUser}/>
                             </form>
                         </div>
-                        <p>Already have an account? <a>Log In now</a></p>
+                        <p>Not a member? <a>Sign up now</a></p>
                     </div>
                 </div>
             </div>
