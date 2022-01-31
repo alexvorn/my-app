72912550e23df940f876ae7be0ea505f628c5f9a
Alexvorn <voroncov.sasha93@gmail.com>
2022-01-31 20:42:22 +0200

    refactoring code in Auth

--- src/reducer/auth.jsx
+++ src/reducer/auth.jsx
@@ -1,18 +1,18 @@
 import { LOGIN, LOGOUT } from '../constants/actionTypes';
 
 export default function authReducer(state = {}, action) {
-    if (action.type === LOGIN) {
-        return {
-            ...state,
-            user: action.payload
-        };
-    };
-    if (action.type === LOGOUT) {
-        return {
-            ...state,
-            user: null,
-        };
-    };
+    switch(action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                user: action.payload
+            };
+        case LOGOUT: 
+            return {
+                ...state,
+                user: null,
+            };
+    }
 
     return state;
 };
